86fd3a2ea4afa9cf203eb2864cf501ccde63b6cf
yumi <robertayuumi@gmail.com>
2023-10-21 19:26:57 -0300

    change of plans - restructuring the page

--- script/index.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-    let openedMenu = document.getElementById('side-menu');
-
-    if (openedMenu.style.width == "0px"){
-        openedMenu.style.width = "auto";
-    } else {
-        openedMenu.style.width = "0px";
-    }
-}
